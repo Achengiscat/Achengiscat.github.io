7ed4078b0a65317f96b8c0d055b470f1b6b50e88
杨鹏程 <1771902961@qq.com>
2022-02-15 23:03:12 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+ let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/j20.jpg') {
+      myImage.setAttribute('src', 'images/laopo.png');
+    } else {
+      myImage.setAttribute('src', 'images/laopo.png');
+    }
+}
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+function setUserName() {
+  let myName = prompt('请输入你的名字。');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = '欢迎你，' + myName;
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+}
+else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = '德华不是笨比，' + storedName;
+}
+myButton.onclick = function() {
+   setUserName();
+}
